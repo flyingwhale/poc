77670dc2cab6ac44b26fd70e1c0ed6104b02f7d0
Imre Toth <tothimre@gmail.com>
2012-01-27 22:02:24 +0100

    Added some docimentation to the CIA protector.

--- framework/src/cache/cacheInvalidationProtection/CIAProtector.php
+++ framework/src/cache/cacheInvalidationProtection/CIAProtector.php
@@ -7,6 +7,28 @@ use POC\core\OptionAble;
 
 use POC\cache\tagging\driver\mysql\model\Cache;
 
+/**
+ * This calss name comes form the "Cache Invalidation Attack Protection" name.
+ * This integrates transpanently to the framework.
+ * 
+ * The basic idea was to implement a subsytem to the framework that protects
+ * the system that uses against the high load if the caches are invalidated, or
+ * just cannot afford long TTL's for the cache, so the pages are generated offten.
+ * 
+ * Alought it can be used in many scenatrios. For instance if you have a page
+ * thats generation takes a long time you can use cache with even very sort ttl 
+ * the cache. If there are lot of concurrent request waits for the page that 
+ * generates for a long time this system can reduce the load from your server 
+ * effectively as well by forcing the clients to wait while the first user's
+ * output is generated. If it is done the clients in the queue will receive the
+ * result. Also we can set up how much client can wait for the results with the 
+ * sleep php method per page, if there are more requests are coming the clients
+ * will be informed about the heavy losad and their client will try to reconnect
+ * to the resource within a second again. 
+ * 
+ * @author Imre Toth
+ *
+ */
 class CIAProtector implements OptionAbleInterface
 {
   const KEY_POSTFIX = "ci";
@@ -106,9 +128,6 @@ class CIAProtector implements OptionAbleInterface
     </BODY> 
     </HTML>';  
   }
-  
-  
-  
 }
 
 ?>

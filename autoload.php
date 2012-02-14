1ef640d4f11890738da7fdec9b1d8d49842ff922
Zsolt Jávorszky <zsolt.javorszky@gmail.com>
2012-02-14 22:02:27 +0100

    Fixed autoload of unittest.

--- autoload.php
+++ autoload.php
@@ -1,18 +1,14 @@
 <?php
-/*Copyright 2012 Imre Toth <tothimre at gmail>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/*
+ * Copyright 2012 Imre Toth <tothimre at gmail> Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
 require_once 'vendor/.composer/autoload.php';
 
@@ -20,11 +16,7 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader ();
 
-$namespaces = array ('Poc\\Tests' =>  __DIR__  , 
-                     'Poc' => __DIR__ . '/src' );
-$loader->registerNamespaces ( $namespaces );
-
-$prefixes = array('Rediska' => __DIR__.'/vendor/rediska/library');
-$loader->registerPrefixes($prefixes);
+$prefixes = array ('Rediska' => __DIR__ . '/vendor/rediska/library' );
+$loader->registerPrefixes ( $prefixes );
 
 $loader->register ();

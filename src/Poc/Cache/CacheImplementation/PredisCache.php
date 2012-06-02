ac46a6db0d82367a70176c8dad5d9009f1f954c5
Zsolt Jávorszky <zsolt.javorszky@gmail.com>
2012-06-02 17:13:34 +0200

    Added Predis cache implementation to project.

---
+++ src/Poc/Cache/CacheImplementation/PredisCache.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Copyright 2011 Imre Toth <tothimre at gmail> Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/**
+ * This class utilizes the REDIS key-value database for caching.
+ *
+ * @author Imre Toth
+ *
+ */
+
+namespace Poc\Cache\CacheImplementation;
+
+use Poc\Core\Optionable\OptionAble;
+
+class PredisCache extends Cache
+{
+
+    protected $redis;
+
+    protected $isNotConnected;
+
+    public function fillDefaults ()
+    {
+        parent::fillDefaults();
+        $this->optionAble['servers'] = array('servers' => array(array('host' => 'localhost', 'port' => '6379')));
+    }
+
+    public function __construct ($options = array())
+    {
+        parent::__construct($options);
+
+        $className = 'Predis\Client';
+        if (! class_exists($className)) {
+            throw new \Exception(sprintf("%s class not exists", $className));
+        }
+
+        $this->redis = new $className($this->optionAble->getOption('servers'));
+        $this->isNotConnected = 1;
+    }
+
+    public function fetch ($key)
+    {
+        $value = $this->redis->get($key);
+        
+        if (is_array($value)) {
+            $value = serialize($value);
+        }
+
+        return $value;
+    }
+
+    public function clearAll ()
+    {
+        $this->redis->flushdb();
+    }
+
+    public function clearItem ($key)
+    {
+        $this->redis->del($key);
+    }
+
+    public function cacheSpecificStore ($key, $output)
+    {
+        $this->redis->set($key, $output);
+        $this->redis->expire($key, $this->ttl);
+    }
+
+    public function isCacheAvailable ()
+    {
+        return $this->isNotConnected;
+    }
+
+}

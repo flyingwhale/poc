667aaf9861908f6f3a6f6a0c75a68f3f5b63d1f3
Imre Toth <tothimre@gmail.com>
2013-03-24 23:54:11 +0100

    fixes issue #3

--- tests/Poc/Tests/Cache/CacheImplementation/CacheTest.php
+++ tests/Poc/Tests/Cache/CacheImplementation/CacheTest.php
@@ -40,6 +40,8 @@ abstract class CacheTest extends \PHPUnit_Framework_TestCase
     public function testCacheSpecificFetch ()
     {
         $this->cache->cacheSpecificStore($this->TESTKEY, self::TESTDATA);
+        //this is for mongodb, sometimes mongodb.
+        usleep(500);
         $fetchedFromCache = $this->cache->fetch($this->TESTKEY);
         $this->assertEquals($fetchedFromCache, self::TESTDATA);
         sleep(self::TTL + 1);
